7880b2b2b8c8a07f7846493c2e39807e55d57b73
Iqra <iqrachoudry92@gmail.com>
2022-02-20 16:47:23 -0500

    git ignore updated, git practice updated

--- src/gitPractice/GitPractice.java
+++ src/gitPractice/GitPractice.java
@@ -5,6 +5,7 @@ public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("My Name is TalentTech");
 		System.out.println("What is your name");
+		System.out.println("My Name is Iqra");
 	}
 
 }
